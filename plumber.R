d6d6110295f560c3f74f2f02ed56c193ded3f7c6
weizlogy <dreamfragment623+weizlogy@gmail.com>
2019-02-17 04:16:30 +0900

    要らない文字の定義を修正

--- plumber.R
+++ plumber.R
@@ -53,7 +53,12 @@ get_my_tweets_terms <- function() {
     tweets1, is.na(retweet_status_id) & is.na(reply_to_status_id) & !str_detect(text, "AUTO-[PREDIC|MARKOV-]"))
 
   # tweetのテキスト抽出 > 要らない文字を消す > tibble形式 > dataframe形式 > めかぶ
-  test <- tweets$text %>% str_replace_all("\\p{ASCII}|[ｦ-ﾟ]", "") %>% enframe(name = NULL, value = "text") %>% as.data.frame %>% RMeCabDF %>% unlist %>% data.frame(., names(.))
+  # 正規表現で除外
+  #   半角カナ, パンクチュエーション, 全角記号, あるぱか, URL, ハッシュタグ
+  test <- tweets$text %>%
+    str_replace_all("[ｦ-ﾟ]|[[:punct:]]|[︰-＠]|[ξ.+?Ҙ]|(https?://t.co/[[:alnum:]]+)|(#.+? )", "") %>%
+    enframe(name = NULL, value = "text") %>%
+    as.data.frame %>% RMeCabDF %>% unlist %>% data.frame(., names(.))
 
   colnames(test) <- c("Morph", "POS")
 
